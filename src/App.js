3b288d56566b7316c276ea40090a54d6308008f6
OpenShift guest <kkale@rallydev.com>
2013-09-18 10:20:38 -0400

    my 2nd app baseline cloned from exercise

---
+++ src/App.js
@@ -0,0 +1,10 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+  items:{
+        html:'<a href="https://help.rallydev.com/apps/2.0rc1/doc/#!/api/Rally.ui.cardboard.CardBoard">Hint!<a>'
+    },
+
+    launch: function() {
+    }
+});
